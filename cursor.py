a763b5659d3fc100869374925becdf480ca7d5a2
Amol Bhagavathi <abhagavathi21@gmail.com>
2022-11-04 13:49:50 -0500

    Added While Loop to Move Cursor

--- cursor.py
+++ cursor.py
@@ -1,1 +1,14 @@
 import pyautogui as pag
+import random
+import time
+
+
+
+
+while True:
+    x = random.randint(500,1000)
+    y = random.randint(200,800)
+    pag.moveTo(x,y,0.2)
+    time.sleep(5)
+
+
